26d9ad736e5b1f5b3a176a2aaeab6230d11a842b
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-01-17 09:52:14 +0100

    delete unsupported operation

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ export const dataProvider = (
   httpClient: AxiosInstance = axiosInstance
 ): Omit<
   Required<DataProvider>,
-  "createMany" | "updateMany" | "deleteMany"
+  "createMany" | "updateMany" | "deleteMany" | "create" | "update" | "deleteOne" | "custom"
 > => ({
   getList: async ({ resource, pagination, filters, sorters, meta }) => {
     const url = `${apiUrl}/`;
@@ -102,35 +102,6 @@ export const dataProvider = (
     };
   },
 
-  create: async ({ resource, variables, meta }) => {
-    const url = `${apiUrl}/${resource}`;
-
-    const { headers, method } = meta ?? {};
-    const requestMethod = (method as MethodTypesWithBody) ?? "post";
-
-    const { data } = await httpClient[requestMethod](url, variables, {
-      headers,
-    });
-
-    return {
-      data,
-    };
-  },
-
-  update: async ({ resource, id, variables, meta }) => {
-    const url = `${apiUrl}/${resource}/${id}`;
-
-    const { headers, method } = meta ?? {};
-    const requestMethod = (method as MethodTypesWithBody) ?? "patch";
-
-    const { data } = await httpClient[requestMethod](url, variables, {
-      headers,
-    });
-
-    return {
-      data,
-    };
-  },
 
   getOne: async ({ resource, id, meta }) => {
     const url = `${apiUrl}`;
@@ -162,82 +133,8 @@ export const dataProvider = (
     };
   },
 
-  deleteOne: async ({ resource, id, variables, meta }) => {
-    const url = `${apiUrl}/${resource}/${id}`;
-
-    const { headers, method } = meta ?? {};
-    const requestMethod = (method as MethodTypesWithBody) ?? "delete";
-
-    const { data } = await httpClient[requestMethod](url, {
-      data: variables,
-      headers,
-    });
-
-    return {
-      data,
-    };
-  },
-
   getApiUrl: () => {
     return apiUrl;
   },
 
-  custom: async ({
-    url,
-    method,
-    filters,
-    sorters,
-    payload,
-    query,
-    headers,
-  }) => {
-    let requestUrl = `${url}?`;
-
-    if (sorters) {
-      const generatedSort = generateSort(sorters);
-      if (generatedSort) {
-        const { _sort, _order } = generatedSort;
-        const sortQuery = {
-          _sort: _sort.join(","),
-          _order: _order.join(","),
-        };
-        requestUrl = `${requestUrl}&${stringify(sortQuery)}`;
-      }
-    }
-
-    if (filters) {
-      const filterQuery = generateFilter(filters);
-      requestUrl = `${requestUrl}&${stringify(filterQuery)}`;
-    }
-
-    if (query) {
-      requestUrl = `${requestUrl}&${stringify(query)}`;
-    }
-
-    let axiosResponse;
-    switch (method) {
-      case "put":
-      case "post":
-      case "patch":
-        axiosResponse = await httpClient[method](url, payload, {
-          headers,
-        });
-        break;
-      case "delete":
-        axiosResponse = await httpClient.delete(url, {
-          data: payload,
-          headers: headers,
-        });
-        break;
-      default:
-        axiosResponse = await httpClient.get(requestUrl, {
-          headers,
-        });
-        break;
-    }
-
-    const { data } = axiosResponse;
-
-    return Promise.resolve({ data });
-  },
 });
